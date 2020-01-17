a0d7823c0ba8b71cac6d17f2b86d6de7170ba2aa
mitch <mitchell@lantis.com>
2020-01-17 03:04:53 -0500

    commented code added

--- state.cpp
+++ state.cpp
@@ -1,50 +1,45 @@
 // state.cpp
 
-
 #include "headers.h"
 
 #include "state.h"
 
-
 // Draw each of the objects in the world
 
-
-void State::draw() 
+void State::draw()
 
 {
   int i;
 
   gpuProgram->activate();
 
-  glUniformMatrix4fv( glGetUniformLocation( gpuProgram->id(), "M"), 1, GL_TRUE, &M[0][0] );
+  glUniformMatrix4fv(glGetUniformLocation(gpuProgram->id(), "M"), 1, GL_TRUE, &M[0][0]);
 
-  silos[0].draw( gpuProgram );
-  
-  for (i=0; i<silos.size(); i++)
-    silos[i].draw( gpuProgram );
+  silos[0].draw(gpuProgram);
 
-  for (i=0; i<cities.size(); i++)
-    cities[i].draw( gpuProgram );
+  for (i = 0; i < silos.size(); i++)
+    silos[i].draw(gpuProgram);
 
-  for (i=0; i<missilesIn.size(); i++)
-    missilesIn[i].draw( gpuProgram );
+  for (i = 0; i < cities.size(); i++)
+    cities[i].draw(gpuProgram);
 
-  for (i=0; i<missilesOut.size(); i++)
-    missilesOut[i].draw( gpuProgram );
+  for (i = 0; i < missilesIn.size(); i++)
+    missilesIn[i].draw(gpuProgram);
 
-  for (i=0; i<explosions.size(); i++)
-    explosions[i].draw( gpuProgram );
+  for (i = 0; i < missilesOut.size(); i++)
+    missilesOut[i].draw(gpuProgram);
+
+  for (i = 0; i < explosions.size(); i++)
+    explosions[i].draw(gpuProgram);
 
   gpuProgram->deactivate();
 }
-
 
 // Update the state of the world after some time interval, deltaT
 //
 // CHANGE ALL OF THIS FUNCTION
 
-
-void State::updateState( float deltaT )
+void State::updateState(float deltaT)
 
 {
   int i;
@@ -58,37 +53,52 @@ void State::updateState( float deltaT )
   //
   // CHANGE THIS
 
-  if (randIn01() > 0.99) {	// New missile 
-    
-    float randXsrc = randIn01();
-    std:: cout << "SOURCE X: " << randXsrc << std::endl;
+  if (randIn01() > 0.99)
+  { // New missile
 
-    float yGenBound1 = worldTop / (randXsrc) ; //slopes
-    float yGenBound2 = -worldTop / (1 - randXsrc);
-    
-    float randXvec = randIn01();
-    
+    float randXsrc = randIn01();
+    std::cout << "SOURCE X: " << randXsrc << std::endl;
+
+    //the following calculates slopes of lines from (random X source, worldTop) to (0, 0) and (0, 1)
+    //This so for any randomly generated x component of missile vector a Y component can be generated
+    // such that the resulting vector will terminate within the screen dimensions on the line y = 0
+
+    float yGenBound1 = worldTop / (randXsrc); //slope of line from src to 0,0
+    float yGenBound2 = -worldTop / (1 - randXsrc); // slope of line from src to 0,1
+
+    float randXvec = randIn01(); 
     float randYvec = 0;
-    
-    if (randXvec < randXsrc){
+
+    if (randXvec < randXsrc)
+    { 
+      // if x component of vector is left of source X coord
       randYvec = yGenBound1 * randXvec * randIn01();
-    } else if (randXvec > randXsrc){
+    }
+    else if (randXvec > randXsrc)
+    {
+      // if x component of vector is right of source X coord
       randYvec = (yGenBound2 * (randXvec - randXsrc) + worldTop) * randIn01();
     }
-    std:: cout << "x: " << randXvec;
-    std:: cout << " y: " << randYvec << std::endl;
-    //0.67 / -(randXsrc) ;
+    else
+    {
+      //if it is the same as the source x coord
+      randYvec = worldTop * randIn01();
+    }
+    std::cout << "x: " << randXvec;
+    std::cout << " y: " << randYvec << std::endl;
+    
 
-    missilesIn.add( Missile( vec3( randXsrc, worldTop, 0), // source
-			     0.4 * vec3( randXvec - randXsrc, randYvec - worldTop, 0 ).normalize(),         // velocity
-			     0,                              // destination y
-			     vec3( 1,1,0 ) ) );              // colour
+    missilesIn.add(Missile(vec3(randXsrc, worldTop, 0),                                         // source
+                           0.4 * vec3(randXvec - randXsrc, randYvec - worldTop, 0).normalize(), // velocity
+                           0,                                                                   // destination y
+                           vec3(1, 1, 0)));                                                     // colour
   }
 
   // Look for terminating missiles
 
-  for (i=0; i<missilesIn.size(); i++)
-    if (missilesIn[i].hasReachedDestination()) {
+  for (i = 0; i < missilesIn.size(); i++)
+    if (missilesIn[i].hasReachedDestination())
+    {
       // CHANGE THIS: ADD AN EXPLOSION
 
       //explosions.add(new Circle(vec3 p, float s, float maxRad, vec3 c));
@@ -96,8 +106,9 @@ void State::updateState( float deltaT )
       i--;
     }
 
-  for (i=0; i<missilesOut.size(); i++)
-    if (missilesOut[i].hasReachedDestination()) {
+  for (i = 0; i < missilesOut.size(); i++)
+    if (missilesOut[i].hasReachedDestination())
+    {
       // CHANGE THIS: ADD AN EXPLOSION
       missilesOut.remove(i);
       i--;
@@ -105,8 +116,9 @@ void State::updateState( float deltaT )
 
   // Look for terminating explosions
 
-  for (i=0; i<explosions.size(); i++)
-    if (explosions[i].radius() >= explosions[i].maxRadius()) {
+  for (i = 0; i < explosions.size(); i++)
+    if (explosions[i].radius() >= explosions[i].maxRadius())
+    {
       // CHANGE THIS: CHECK FOR DESTROYED CITY OR SILO
       explosions.remove(i);
       i--;
@@ -119,41 +131,38 @@ void State::updateState( float deltaT )
 
   // Update all the moving objects
 
-  for (i=0; i<missilesIn.size(); i++)
-    missilesIn[i].move( deltaT );
+  for (i = 0; i < missilesIn.size(); i++)
+    missilesIn[i].move(deltaT);
 
-  for (i=0; i<missilesOut.size(); i++)
-    missilesOut[i].move( deltaT );
+  for (i = 0; i < missilesOut.size(); i++)
+    missilesOut[i].move(deltaT);
 
-  for (i=0; i<explosions.size(); i++)
-    explosions[i].expand( deltaT );
+  for (i = 0; i < explosions.size(); i++)
+    explosions[i].expand(deltaT);
 }
-
 
 // Fire a missile
 
-void State::fireMissile( int siloIndex, float x, float y )
+void State::fireMissile(int siloIndex, float x, float y)
 
 {
   const float speed = 0.3;
-    
-  if (silos[siloIndex].canShoot()) {
+
+  if (silos[siloIndex].canShoot())
+  {
 
     silos[siloIndex].decrMissiles();
 
     // CHANGE THIS
 
-    missilesOut.add( Missile( silos[siloIndex].position(),           // source
-			      speed * vec3(x - silos[siloIndex].position().x, y - silos[siloIndex].position().y, 0).normalize(), // velocity
-			      y,		                     // destination y
-			      vec3( 0,1,1 ) ) );                     // colour
+    missilesOut.add(Missile(silos[siloIndex].position(),                                                                       // source
+                            speed * vec3(x - silos[siloIndex].position().x, y - silos[siloIndex].position().y, 0).normalize(), // velocity
+                            y,                                                                                                 // destination y
+                            vec3(0, 1, 1)));                                                                                   // colour
   }
 }
 
-
-
 // Create the initial state of the world
-
 
 void State::setupWorld()
 
@@ -162,34 +171,34 @@ void State::setupWorld()
 
   currentTime = 0;
 
-  timeOfIncomingFlight = 6;	// 6 seconds for incoming missile to arrive
+  timeOfIncomingFlight = 6; // 6 seconds for incoming missile to arrive
 
   // Seed the random number generator
 
 #ifdef _WIN32
-  srand( 12345 );
+  srand(12345);
 #else
   struct timeb t;
-  ftime( &t );
-  srand( t.time );
+  ftime(&t);
+  srand(t.time);
 #endif
 
   // Silos are added in order left, middle, right so that they have
   // indices 0, 1, and 2 (respectively) in the "silos" array.
 
-  silos.add(  Silo( vec3( 0.1, 0, 0 ) ) );
-  silos.add(  Silo( vec3( 0.5, 0, 0 ) ) );
-  silos.add(  Silo( vec3( 0.9, 0, 0 ) ) );
+  silos.add(Silo(vec3(0.1, 0, 0)));
+  silos.add(Silo(vec3(0.5, 0, 0)));
+  silos.add(Silo(vec3(0.9, 0, 0)));
 
   // Cities on the left
 
-  cities.add( City( vec3( 0.2, 0, 0 ) ) );
-  cities.add( City( vec3( 0.3, 0, 0 ) ) );
-  cities.add( City( vec3( 0.4, 0, 0 ) ) );
+  cities.add(City(vec3(0.2, 0, 0)));
+  cities.add(City(vec3(0.3, 0, 0)));
+  cities.add(City(vec3(0.4, 0, 0)));
 
   // Cities on the right
 
-  cities.add( City( vec3( 0.6, 0, 0 ) ) );
-  cities.add( City( vec3( 0.7, 0, 0 ) ) );
-  cities.add( City( vec3( 0.8, 0, 0 ) ) );
+  cities.add(City(vec3(0.6, 0, 0)));
+  cities.add(City(vec3(0.7, 0, 0)));
+  cities.add(City(vec3(0.8, 0, 0)));
 }
